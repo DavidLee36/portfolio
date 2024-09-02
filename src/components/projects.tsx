88097bfd8061c53e2184dd79c166c29966f7143d
DavidLee36 <104027396+DavidLee36@users.noreply.github.com>
2024-09-02 13:28:15 -0700

    Projects styling

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -5,23 +5,25 @@ import '../styles/main.css';
 export const Projects = () => {
     return (
         <div className="projects-container">
+            <div className="projects-heading">
+                <h1 className="projects-container-title">Projects</h1>
+                <h3>Below is a list of projects that I have created, this is not a complete list of every project I've made</h3>
+            </div>
             {projectList.map((project, idx) => (
                 <div className="project" key={idx}>
-                    <div className="project-main-content">
-                        <img src={project.img.src} alt={project.img.alt} className="project-img" />
-                        <div className="project-text">
-                            <h2 className="project-title">
-                                {project.title}
-                            </h2>
-                            <p className="project-desc">
-                                {project.desc}
-                            </p>
+                    <img src={project.img.src} alt={project.img.alt} className="project-img" />
+                    <div className="project-text">
+                        <h2 className="project-title">
+                            {project.title}
+                        </h2>
+                        <h3 className="project-desc">
+                            {project.desc}
+                        </h3>
+                        <div className="project-technologies">
+                            {project.techList.map((tech, index) => (
+                                <img src={`/assets/svg/technologies/${tech}.svg`} alt={tech} className="project-tech-img" key={index} />
+                            ))}
                         </div>
-                    </div>
-                    <div className="project-technologies">
-                        {project.techList.map((tech, index) => (
-                            <img src={`/assets/svg/technologies/${tech}.svg`} alt={tech} className="project-tech-img" key={index}/>
-                        ))}
                     </div>
                 </div>
             ))}
