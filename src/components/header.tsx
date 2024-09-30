b6f5a5e5e11f68b0bc1c60020ea7da03db388a60
DavidLee36 <104027396+DavidLee36@users.noreply.github.com>
2024-09-30 11:30:10 -0700

    updated resume link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,7 +12,7 @@ export const Header = () => {
                     <h2>Software Engineer</h2>
                     <h3><br />I'm a software engineer with a Bachelor's degree in Computer Science from <a href='https://www.cwu.edu/academics/computer-science/'> Central Washington University</a>, class of 2023. I have a strong passion for coding and problem-solving, with experience in software development, embedded systems, CAD, and electronics. I love tackling new challenges, continuously expanding my skills, and working on innovative projects that push the boundaries of technology.</h3>
                     {links()}
-                    <a href="/assets/Resume.pdf" target='_blank' className='resume-link'>
+                    <a href="/assets/DavidLeeResume.pdf" target='_blank' className='resume-link'>
                         <h2 className="resume">
                             View Resume
                             <svg className='arrow resume-svg' viewBox='0 0 24 24'>
